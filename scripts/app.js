1c4e975e3cee8558ca1f4ea750cbdd76542db5fb
YOUR_GITHUB_USERNAME <garrickdark@yahoo.com>
2017-07-06 22:12:50 -0700

    v1

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,34 @@
-$(document).on("ready", function(){
+console.log("sanity check")
 
+function renderGifs() {
+  $.ajax({
+    method:'GET',
+    url:'http://api.giphy.com/v1/gifs/search',
+    data: $(".form-inline").serialize(),
+    success: onSuccess,
+    error: onError,
+  });
+}
+
+$(document).on("ready", function(){
+    renderGifs();
+    $('.form-inline').on('submit',function(event){
+      event.preventDefault();
+      renderGifs();
+    });
 });
+
+function onSuccess(json){
+  // json.data.forEach(function(gif){
+  //   console.log(gif.images.fixed_height.url)
+  // })
+  // $(".gif-image").remove();
+  console.log(json.data)
+  json.data.forEach(function(gif) {
+    $(".gif-gallery").append("<img class='gif-image' src=" + gif.images.fixed_height.url + ">");
+  });
+};
+
+function onError(xhr, status, errorThrown){
+  console.log(xhr, status, errorThrown)
+};
